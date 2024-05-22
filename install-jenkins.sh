10d9aa3babb2aa1f35ce28ea43d0adf5fcfac458
Marcos Monteiro <marcos-monteiro@pluralsight.com>
2024-05-22 15:52:09 +1000

    Update Jenkins commands

--- install-jenkins.sh
+++ install-jenkins.sh
@@ -20,5 +20,5 @@ wget -O /usr/share/keyrings/jenkins-keyring.asc https://pkg.jenkins.io/debian-st
 echo "deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc]" https://pkg.jenkins.io/debian-stable binary/ | sudo tee /etc/apt/sources.list.d/jenkins.list > /dev/null
 sudo apt-get update -y
 sudo apt-get install -y jenkins
-sudo touch /var/lib/jenkins/jenkins.install.InstallUtil.lastExecVersion
-sudo systemctl restart jenkins
+sudo systemctl enable jenkins
+sudo systemctl start jenkins
